aa45d9426519aaa9de17f29a6b732903d75257c8
jonathanjross <jonathan.ross@uqconnect.edu.au>
2022-02-08 11:05:23 +1000

    finished

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <div>
+      <span v-for="area in areas" v-bind:key="area">{{ area }}</span>
+    </div>
+<div class="action">
+    <router-link v-bind:to="coachContactLink">Contact</router-link>
+    <router-link v-bind:to="coachDetailsLink">View Details</router-link>
+</div>
+  </li>
+</template>
+
+<script>
+export default {
+    props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
+    computed: {
+        fullName() {
+            return this.firstName + ' ' + this.lastName;
+        },
+        coachContactLink() {
+            return this.$route.path + '/' + '/contact' // /coaches/c1/contact
+        },
+        coachDetailsLink() {
+            return '/coaches/' + this.id;
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
